aa630a6df510fa12cf2a7842bdb56a2ac9b05ac6
Peter Kraml <piets@macpiets.net>
2022-11-09 16:26:39 +0100

    Add fakeapi for local testing

---
+++ fakeapi/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3210;
+
+let temperature = 21.5;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send(JSON.stringify({temperature: temperature}));
+});
+
+app.post('/', (req, res) => {
+	temperature = req.body.temperature;
+	console.log(`Set Temperature to ${temperature}`);
+	res.send(JSON.stringify({temperature: temperature}));
+});
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`)
+})
